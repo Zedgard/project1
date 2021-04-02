feb21a65a12c21072c547a463c2ad0b0c0fe15c4
DESKTOP-WIN10I\Viktor <koman1706@gmail.com>
2021-04-03 01:43:54 +1000

    index

--- themes/site1/index_ru.php
+++ themes/site1/index_ru.php
@@ -9,7 +9,6 @@
         <link href="/favicon.ico<?= $_SESSION['rand'] ?>" rel="icon">
         <meta name="google-site-verification" content="Sozz79bTt3VOI21yJOn4xH2czaki3n7psELbIxXdI34" />
         <meta name="interkassa-verification" content="777d8a842d5f8a07fe433d7f9e9537fd" />
-        <meta name="robots" content="noindex, nofollow">
         <?= $_SESSION['noindex'] ?>
         
         <!-- SLEEK CSS -->
