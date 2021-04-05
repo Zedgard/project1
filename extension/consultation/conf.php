f3629db2994431228d5794e3988045b696af8315
Viktor <koman1706@gmail.com>
2021-04-06 02:29:26 +1000

    config

---
+++ extension/consultation/conf.php
@@ -0,0 +1,11 @@
+<?php
+
+$config = array(
+    'title' => 'Записаться на консультацию',
+    'descr' => 'Форма Записаться на консультацию',
+    'urls' => array(
+        'Записаться на консультацию admin.php' => '/extension/sign_up_consultation/admin.php',
+        'Записаться на консультацию index.php' => '/extension/sign_up_consultation/index.php'
+    ),
+    'version' => '0.1'
+);
