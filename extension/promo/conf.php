f68def1c3e29f1fe273f63e97c5811623a2c0ffa
Viktor <koman1706@gmail.com>
2021-04-19 01:11:31 +1000

    Промо файлы

---
+++ extension/promo/conf.php
@@ -0,0 +1,10 @@
+<?php
+
+$config = array(
+    'title' => 'Записаться на консультацию новая форма',
+    'descr' => 'Форма Записаться на консультацию новая форма',
+    'urls' => array(
+        'Записаться на консультацию NEW index.php' => '/extension/consultation/index.php'
+    ),
+    'version' => '0.1'
+);
