dbc5eeb13c8e8f39598b9bd64819834ca4553108
Viktor <koman1706@gmail.com>
2021-04-25 18:03:38 +1000

    Сортировка в кабинете

--- extension/products/js/office.js
+++ extension/products/js/office.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
  */
 function getClientProducts() {
     $(".products_arrays_data div").remove();
-    $(".products_arrays_data").html('<div class="w-100 text-center"><div class="spinner-border text-dark" role="status"><span class="sr-only">Loading...</span></div></div>');
+    $(".products_arrays_data").html('<div class="w-100 text-center"><div class="spinner-border text-dark" role="status"><span class="sr-only">Загрузка...</span></div></div>');
     sendPostLigth('/jpost.php?extension=wares', {
         "getClientProducts": '1',
         "category_id": category_id
@@ -17,7 +17,7 @@ function getClientProducts() {
             for (var i = 0; i < data.length; i++) {
                 if (!$(".products_arrays_data").find('.product_id_' + data[i]['product_id'])[0]) {
                     // Основной продукт
-                    $(".products_arrays_data").append('<div class="product product_id_' + data[i]['product_id'] + ' mixitup-container category-' + data[i]['pcat_category_id'] + ' w-100" \n\
+                    $(".products_arrays_data").append('<div class="product product_id_' + data[i]['product_id'] + ' mixitup-container category-' + data[i]['wcat_category_id'] + ' w-100" \n\
                         data-ref="mixitup-target" style="display: block;">\n\
                         <div class="product_title w-100 mt-3 ml-0 ml-lg-3">' + data[i]['product_title'] + '</div>\n\
                         <div class="product_elms row row-cols-2 row-cols-md-4 w-100" style="margin-right:0;padding-right:0;margin-left:0;padding-left:0;"></div>\n\
@@ -37,18 +37,6 @@ function getClientProducts() {
                                 </a>\n\
                                 </div>\n\
                                 ');
-                // <div class="col-lg-8 mb-3">' + data[i]['descr'] + '</div>
-                //$(".products_arrays_data").append('<div class="col-lg-12 mb-2"><hr/></div>');
-//                    $(".products_arrays_data tbody").append(
-//                            '<tr elm_id="' + data[i]['id'] + '"> \n\
-//                                <td>\n\
-//                                <div class="mb-3"><a href="?wares_id=' + data[i]['id'] + '">' + data[i]['title'] + '</a></div>\n\
-//                                <div class="mb-3 text-center"><img src="' + data[i]['images'] + '" style="max-width: 200px;max-height: 160px;"/></div>\n\
-//                                </td>\n\
-//                                <td><div>' + data[i]['descr'] + '</div>\n\
-//                                </td>\n\
-//                            </tr>');
-                //  <div class="text-right"><a href="?product_id=' + data[i]['id'] + '" class="p-2 bd-highlight btn btn-primary">Перейти</a></div>\n\
             }
             /*
              * Активируем фильтр
@@ -56,7 +44,6 @@ function getClientProducts() {
             setTimeout(function () {
                 var containerEl = document.querySelector('[data-ref~="mixitup-container"]');
                 if (!!containerEl) {
-                    //console.log('containerEl');
                     var mixer = mixitup(containerEl, {
                         selectors: {
                             target: '[data-ref~="mixitup-target"]'
