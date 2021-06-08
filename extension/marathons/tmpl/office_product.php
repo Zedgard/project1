2e77c9bbeaabee524714d165859b3e2a2ccbc088
Viktor <koman1706@gmail.com>
2021-06-08 18:08:33 +1000

    Оптимизация

--- extension/marathons/tmpl/office_product.php
+++ extension/marathons/tmpl/office_product.php
@@ -6,95 +6,104 @@
 <script src="/assets/plugins/video/Youtube.js<?= $_SESSION['rand'] ?>"></script>
 <input type="hidden" name="marathons_wares_id" value="<?= $_GET['wares_id'] ?>" class="marathons_wares_id" />
 <div class="container-fluid">
-    <!-- Большие экраны d-none d-lg-block -->
-    <div class="">
-        <div style="height: 10px;"></div>
-        <div class="marathons_wares_title">Прохождение марафона &laquo; <?= $wares['title'] ?> &raquo;</div>
-        <div class="row mb-3">
-            <div class="col-md mb-1">
-                <div class="marathons_elm">
-                    <a href="javascript:void(0)" 
-                       class="d-none d-lg-block marathons_btn marathons_btn_green marathons_material_series_btn" 
-                       series_id="0" 
-                       elm_type="D"
-                       >
-                        <span>Общие материалы марафона</span>
-                    </a>
-                    <a href="javascript:void(0)" 
-                       class="d-block d-lg-none marathons_btn marathons_btn_green marathons_material_series_btn" 
-                       series_id="0" 
-                       elm_type="M"
-                       >
-                        <span>Общие материалы марафона</span>
-                    </a>
-                </div>
-            </div>
-            <div class="col-md mb-1" style="<?= $bonus_btn_style ?>">
-                <div class="marathons_elm">
-                    <a href="javascript:void(0)" 
-                       class="d-none d-lg-block marathons_btn marathons_btn_brown marathons_material_series_btn" 
-                       bonus_material_id="<?= $bonus_material_id ?>"
-                       elm_type="D"
-                       >
-                        <span>Бонус</span> <?= $bonus_lock_content ?>
-                    </a>
-                    <a href="javascript:void(0)" 
-                       class="d-block d-lg-none marathons_btn marathons_btn_brown marathons_material_series_btn" 
-                       bonus_material_id="<?= $bonus_material_id ?>"
-                       elm_type="M"
-                       >
-                        <span>Бонус</span> <?= $bonus_lock_content ?>
-                    </a>
-                </div>
-            </div>
+    <div class="row">
+        <div class="col-lg">
+            <div class="marathons_wares_title"><span style="display: none;">Прохождение марафона &laquo; <?= $wares['title'] ?> &raquo;</span><?= $wares['title'] ?></div>
         </div>
-        <div class="row mb-3">
-            <div class="col-12">
-                <?
-                foreach ($series as $series_value) {
-                    if ($series_value['series_enable'] == 1) {
-                        if (mb_strtoupper($series_value['title']) == 'БОНУС') {
-                            continue;
+        <div class="col-lg">
+
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-lg">
+
+            <div class="row mb-3">
+                <div class="col-md mb-1">
+                    <div class="marathons_elm">
+                        <a href="javascript:void(0)" 
+                           class="d-none d-lg-block marathons_btn marathons_btn_green marathons_material_series_btn ckick_to_upload_page" 
+                           series_id="0" 
+                           elm_type="D"
+                           >
+                            <span>Общие материалы марафона</span>
+                        </a>
+                        <a href="javascript:void(0)" 
+                           class="d-block d-lg-none marathons_btn marathons_btn_green marathons_material_series_btn ckick_to_upload_page" 
+                           series_id="0" 
+                           elm_type="M"
+                           >
+                            <span>Общие материалы марафона</span>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-md mb-1" style="<?= $bonus_btn_style ?>">
+                    <div class="marathons_elm">
+                        <a href="javascript:void(0)" 
+                           class="d-none d-lg-block marathons_btn marathons_btn_brown marathons_material_series_btn" 
+                           bonus_material_id="<?= $bonus_material_id ?>"
+                           elm_type="D"
+                           >
+                            <span>Бонус</span> <?= $bonus_lock_content ?>
+                        </a>
+                        <a href="javascript:void(0)" 
+                           class="d-block d-lg-none marathons_btn marathons_btn_brown marathons_material_series_btn" 
+                           bonus_material_id="<?= $bonus_material_id ?>"
+                           elm_type="M"
+                           >
+                            <span>Бонус</span> <?= $bonus_lock_content ?>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="row mb-3">
+                <div class="col-12">
+                    <?
+                    foreach ($series as $series_value) {
+                        if ($series_value['series_enable'] == 1) {
+                            if (mb_strtoupper($series_value['title']) == 'БОНУС') {
+                                continue;
+                            }
+                            ?>
+                            <div class="marathons_elm mb-1">
+                                <a href="javascript:void(0)" 
+                                   class="d-none d-lg-block marathons_btn marathons_btn_green marathons_material_series_btn <?= $bonus_class ?>" 
+                                   style="<?= $bonus_style ?>" 
+                                   series_id="<?= $series_value['id'] ?>"
+                                   elm_type="D"
+                                   >
+                                    <span><?= $series_value['title'] ?></span><i class="fas fa-plus"></i>
+                                </a>
+                                <a href="javascript:void(0)" 
+                                   class="d-block d-lg-none marathons_btn marathons_btn_green marathons_material_series_btn <?= $bonus_class ?>" 
+                                   style="<?= $bonus_style ?>" 
+                                   series_id="<?= $series_value['id'] ?>"
+                                   elm_type="M"
+                                   >
+                                    <span><?= $series_value['title'] ?></span><i class="fas fa-plus"></i>
+                                </a>
+                            </div>
+                            <?
+                        } else {
+                            ?>
+                            <div class="row mb-1">
+                                <div class="marathons_not_btn marathons_btn_enable_day_lock" series_id="<?= $series_value['id'] ?>"><?= $series_value['title'] ?><i class="fas fa-lock"></i></div>
+                            </div>
+                            <?
                         }
-                        ?>
-                        <div class="marathons_elm mb-1">
-                            <a href="javascript:void(0)" 
-                               class="d-none d-lg-block marathons_btn marathons_btn_green marathons_material_series_btn <?= $bonus_class ?>" 
-                               style="<?= $bonus_style ?>" 
-                               series_id="<?= $series_value['id'] ?>"
-                               elm_type="D"
-                               >
-                                <span><?= $series_value['title'] ?></span><i class="fas fa-plus"></i>
-                            </a>
-                            <a href="javascript:void(0)" 
-                               class="d-block d-lg-none marathons_btn marathons_btn_green marathons_material_series_btn <?= $bonus_class ?>" 
-                               style="<?= $bonus_style ?>" 
-                               series_id="<?= $series_value['id'] ?>"
-                               elm_type="M"
-                               >
-                                <span><?= $series_value['title'] ?></span><i class="fas fa-plus"></i>
-                            </a>
-                        </div>
-                        <?
-                    } else {
-                        ?>
-                        <div class="row mb-1">
-                            <div class="marathons_not_btn marathons_btn_enable_day_lock" series_id="<?= $series_value['id'] ?>"><?= $series_value['title'] ?><i class="fas fa-lock"></i></div>
-                        </div>
-                        <?
                     }
-                }
-                ?>
+                    ?>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="col-md mb-1 materials_list">
+        <div class="col-lg">    
+            <div class="col-md mb-1 materials_list">
+
+            </div>
+        </div>
 
     </div>
-
-
 </div>
-
 <script>
     var bonus_lock = '<?= $bonus_lock ?>';
     var init_series_id = '';
