9d2c4c806adb848d68af586e301a9536d5fc1db5
Viktor <koman1706@gmail.com>
2021-06-21 16:45:40 +1000

    Файлы тренингов

---
+++ extension/online_trenings/conf.php
@@ -0,0 +1,10 @@
+<?php
+
+$config = array(
+    'title' => 'Марафоны',
+    'descr' => 'Марафоны',
+    'urls' => array(
+        'Марафоны для кабинета пользователя' => '/extension/marathons/office.php',
+    ),
+    'version' => '0.1'
+);
