2771c17e402e352acc1d57e8c9b6398d956fac11
Viktor <koman1706@gmail.com>
2021-06-30 09:59:19 +1000

    Отчет

--- upload/site_export_pay.php
+++ upload/site_export_pay.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
  * Експорт продаж для бухгалтера
  */
@@ -13,13 +12,9 @@ $sqlLight = new \project\sqlLight();
 
 $user = new \project\user();
 
-if (!$user->isClient()) {
-    header($_SERVER["SERVER_PROTOCOL"] . " 404 Not Found", true, 404);
-    location_href('/');
-    exit();
-}
-$from = (isset($_GET['from']) && strlen($_GET['from']) > 0) ? trim($_GET['from']) : '';
-$to = (isset($_GET['to']) && strlen($_GET['to']) > 0) ? trim($_GET['to']) : '';
+/*
+ * Функции
+ */
 
 // Шапка
 function download_send_headers($filename) {
@@ -61,44 +56,53 @@ function date_sql_format($date_str) {
     return $date_new;
 }
 
-if (strlen($from) > 0 && strlen($to) > 0) {
-    //print_r($_SESSION['admin_pay_filter']);
-    $from = date_sql_format($_SESSION['admin_pay_filter']['excel_from']);
-    $to = date_sql_format($_SESSION['admin_pay_filter']['excel_to']);
+/*
+ * Процесс
+ */
+if ($user->isEditor()) {
+    $from = (isset($_GET['from']) && strlen($_GET['from']) > 0) ? trim($_GET['from']) : '';
+    $to = (isset($_GET['to']) && strlen($_GET['to']) > 0) ? trim($_GET['to']) : '';
 
-    $search_pay_user_str = $_SESSION['admin_pay_filter']['search_pay_user_str'];
-    $pay_search_type = $_SESSION['admin_pay_filter']['pay_search_type'];
-    $pay_search_status = $_SESSION['admin_pay_filter']['pay_search_status'];
 
-    $queryArray = array();
-    $w = array();
+
     if (strlen($from) > 0 && strlen($to) > 0) {
-        $queryArray[] = $from;
-        $queryArray[] = $to;
-        $w[] = "p.pay_date BETWEEN '? 00:00:00' and '? 23:59:59' ";
-    }
-    if (strlen($pay_search_type) > 0) {
-        $queryArray[] = $pay_search_type;
-        $w[] = "p.pay_type='?'";
-    }
-    if (strlen($pay_search_status) > 0) {
-        $queryArray[] = $pay_search_status;
-        $w[] = "p.pay_status='?'";
-    }
-    if (strlen($search_pay_user_str) > 0) {
-        $queryArray[] = $search_pay_user_str;
-        $queryArray[] = $search_pay_user_str;
-        $w[] = "(u.email like '%?%' or u.phone like '%?%') "; //pt.pay_type_title like '%?%'
-    }
-    //print_r($w);
-    if (count($w) > 0) {
-        $where = 'WHERE ' . implode(' and ', $w);
-    }
+        //print_r($_SESSION['admin_pay_filter']);
+        $from = date_sql_format($_SESSION['admin_pay_filter']['excel_from']);
+        $to = date_sql_format($_SESSION['admin_pay_filter']['excel_to']);
 
-    $query = "SELECT p.id, pt.pay_type_title, "
-            . "u.email, u.phone, u.first_name, u.last_name, "
-            . "p.pay_sum, p.pay_date, p.payment_bank, p.pay_status, p.pay_descr,"
-            . " (
+        $search_pay_user_str = $_SESSION['admin_pay_filter']['search_pay_user_str'];
+        $pay_search_type = $_SESSION['admin_pay_filter']['pay_search_type'];
+        $pay_search_status = $_SESSION['admin_pay_filter']['pay_search_status'];
+
+        $queryArray = array();
+        $w = array();
+        if (strlen($from) > 0 && strlen($to) > 0) {
+            $queryArray[] = $from;
+            $queryArray[] = $to;
+            $w[] = "p.pay_date BETWEEN '? 00:00:00' and '? 23:59:59' ";
+        }
+        if (strlen($pay_search_type) > 0) {
+            $queryArray[] = $pay_search_type;
+            $w[] = "p.pay_type='?'";
+        }
+        if (strlen($pay_search_status) > 0) {
+            $queryArray[] = $pay_search_status;
+            $w[] = "p.pay_status='?'";
+        }
+        if (strlen($search_pay_user_str) > 0) {
+            $queryArray[] = $search_pay_user_str;
+            $queryArray[] = $search_pay_user_str;
+            $w[] = "(u.email like '%?%' or u.phone like '%?%') "; //pt.pay_type_title like '%?%'
+        }
+        //print_r($w);
+        if (count($w) > 0) {
+            $where = 'WHERE ' . implode(' and ', $w);
+        }
+
+        $query = "SELECT p.id, pt.pay_type_title, "
+                . "u.email, u.phone, u.first_name, u.last_name, "
+                . "p.pay_sum, p.pay_date, p.payment_bank, p.pay_status, p.pay_descr,"
+                . " (
                 SELECT
                     GROUP_CONCAT(pr.title)
                 FROM
@@ -108,27 +112,32 @@ if (strlen($from) > 0 && strlen($to) > 0) {
                 WHERE
                     pps.pay_id = p.id
             ) AS product_list "
-            . "FROM zay_pay p "
-            . "left join zay_pay_type pt on pt.pay_type_code=p.pay_type "
-            . "left join zay_users u on u.id=p.user_id "
-            . "{$where} " //where p.pay_date BETWEEN '? 00:00:00' and '? 23:59:59' p.pay_status='succeeded'
-            . "ORDER BY p.pay_date DESC";
+                . "FROM zay_pay p "
+                . "left join zay_pay_type pt on pt.pay_type_code=p.pay_type "
+                . "left join zay_users u on u.id=p.user_id "
+                . "{$where} " //where p.pay_date BETWEEN '? 00:00:00' and '? 23:59:59' p.pay_status='succeeded'
+                . "ORDER BY p.pay_date DESC";
 
 
 
-    $sqlLight->setMysqliAssos();
-    $data = $sqlLight->queryList($query, $queryArray, 0);
-    //exit();
-    if (count($data) > 0) {
-        //foreach ($data as $key => $value) {
-        download_send_headers("data_export___{$from}-{$to}.csv");
-        echo array2csv($data);
+        $sqlLight->setMysqliAssos();
+        $data = $sqlLight->queryList($query, $queryArray, 0);
+        //exit();
+        if (count($data) > 0) {
+            //foreach ($data as $key => $value) {
+            download_send_headers("data_export___{$from}-{$to}.csv");
+            echo array2csv($data);
 
-        //}
+            //}
+        } else {
+            echo 'Записей не найдено!';
+        }
     } else {
-        echo 'Записей не найдено!';
+        echo 'Не верные параметры!';
     }
 } else {
-    echo 'Не верные параметры!';
+    header($_SERVER["SERVER_PROTOCOL"] . " 404 Not Found", true, 404);
+    ?><div>Нет прав доступа для просмотра информации</div><?
+    //location_href('/');
+    exit();
 }
-
