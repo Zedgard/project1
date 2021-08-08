b9048820675d807765b2bc2a660829fa6834bc3e
Viktor <koman1706@gmail.com>
2021-08-08 18:09:41 +1000

    Размер видеоплеера и текст описания товара #131313

--- extension/wares/tmpl/material_type_video.php
+++ extension/wares/tmpl/material_type_video.php
@@ -1,9 +1,22 @@
+<style>
+    .material_video_youtube{
+        width: 100%;
+        min-height: 25rem;
+        margin: 0 auto;
+    }
+    @media (max-width: 768px) {
+        .material_video_youtube{
+            width: 100%;
+            min-height: 11rem;
+            margin: 0 auto;
+        }
+    }
+</style>
 <div class="video_see see_video_<?= $value['id'] ?>">
     <video
         id="video_<?= $union_elm_id ?>"
         class="video-js vjs-default-skin material_video_youtube"
         controls
-        style="width: 100%;min-height: 25rem;margin: 0 auto;"
         data-setup='{ "techOrder": ["youtube", "html5"], "sources": [{ "type": "video/youtube", "src": "<?= $value['video_youtube'] ?>"}] }'
         >
     </video>
@@ -12,14 +25,14 @@
         $(document).ready(function () {
             var options = {};
             //setTimeout(function () {
-                var player = videojs('video_<?= $union_elm_id ?>', options);
+            var player = videojs('video_<?= $union_elm_id ?>', options);
 //                    function onPlayerReady() {
 //                        //this.play();
 //                        this.on('ended', function () {
 //                        });
 //                        //this.pause();
 //                    });
-                player.src({src: '<?= $value['video_youtube'] ?>', type: 'video/youtube'});
+            player.src({src: '<?= $value['video_youtube'] ?>', type: 'video/youtube'});
             //}, 200);
 
 
