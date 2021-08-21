c367fffd24e27ab5e51938ff79f1e0966cc38c6c
Viktor <koman1706@gmail.com>
2021-08-21 12:58:21 +1000

    Перенос закрытого клуба

--- themes/office/menu_left.php
+++ themes/office/menu_left.php
@@ -70,16 +70,16 @@
                 <span class="nav-text">Вебинары</span> 
             </a> 
         </li>
-        <li style="margin: 0;padding: 0;" class="has-sub <?= ($_SESSION['page_url'] == 'shopping') ? 'active' : '' ?> ">
-            <a class="sidenav-item-link" href="/office/shopping/">
-                <i class="mdi mdi-cart"></i>
-                <span class="nav-text">Покупки</span> 
-            </a> 
-        </li>
         <li style="margin: 0;padding: 0;display: block;" class="has-sub <?= ($_SESSION['page_url'] == 'club') ? 'active' : '' ?> ">
             <a class="sidenav-item-link" href="/office/club/">
                 <i class="mdi mdi-cloud-alert"></i>
                 <span class="nav-text">Закрытый<br/>КЛУБ</span> 
+            </a> 
+        </li>
+        <li style="margin: 0;padding: 0;" class="has-sub <?= ($_SESSION['page_url'] == 'shopping') ? 'active' : '' ?> ">
+            <a class="sidenav-item-link" href="/office/shopping/">
+                <i class="mdi mdi-cart"></i>
+                <span class="nav-text">Покупки</span> 
             </a> 
         </li>
         <li style="margin: 0;padding: 0;" class="has-sub <?= ($_SESSION['page_url'] == 'userprofile_admin') ? 'active' : '' ?> ">
