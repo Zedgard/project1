63e65ab27ff29b8d98c81fb61be185c9b6e1b368
Viktor <koman1706@gmail.com>
2021-09-01 17:38:17 +1000

    Исправление авторизации 2

--- extension/users/jpost.php
+++ extension/users/jpost.php
@@ -11,94 +11,94 @@ include_once DOCUMENT_ROOT . '/extension/auth/lang.php';
 $user = new \project\user();
 $auth = new \project\auth();
 
-$_SESSION['user_roles'] = $user->get_roles_array();
-if ($user->isEditor()) {
-    // Данные по пользователю
-    if (isset($_POST['getUsersList'])) {
-        $params = array();
-        $page_num = $_POST['page_num'];
-        $params['input_search_close_club_users'] = (isset($_POST['input_search_close_club_users'])) ? $_POST['input_search_close_club_users'] : 0;
-        $_SESSION['input_search_str'] = $_POST['input_search_str'];
+//$_SESSION['user_roles'] = $user->get_roles_array();
 
-        $user_id = 0;
-        if (isset($_POST['system_user_id']) && $_POST['system_user_id'] > 0) {
-            $user_id = $_POST['system_user_id'];
-            $_SESSION['user_edit_obj_id'] = $user_id;
-        }
+// Данные по пользователю
+if (isset($_POST['getUsersList'])) {
+    $params = array();
+    $page_num = $_POST['page_num'];
+    $params['input_search_close_club_users'] = (isset($_POST['input_search_close_club_users'])) ? $_POST['input_search_close_club_users'] : 0;
+    $_SESSION['input_search_str'] = $_POST['input_search_str'];
 
-        $data = $user->getUserInfo($user_id, $page_num, $_SESSION['input_search_str'], $params);
-        $result = array('success' => 1, 'success_text' => '', 'data' => $data);
+    $user_id = 0;
+    if (isset($_POST['system_user_id']) && $_POST['system_user_id'] > 0) {
+        $user_id = $_POST['system_user_id'];
+        $_SESSION['user_edit_obj_id'] = $user_id;
     }
 
-    // Сохранить данные по пользователю
-    if (isset($_POST['saveProfilSettings'])) {
-        $user_id = $_SESSION['user_edit_obj_id'];
-        $first_name = $_POST['first_name'];
-        $last_name = $_POST['last_name'];
-        $email = $_POST['email'];
-        $login_instagram = $_POST['login_instagram'];
-        $phone = $auth->phoneReplace($_POST['phone']);
-        $user_role_id = $_POST['user_role'];
+    $data = $user->getUserInfo($user_id, $page_num, $_SESSION['input_search_str'], $params);
+    $result = array('success' => 1, 'success_text' => '', 'data' => $data);
+}
 
-        //$oldPassword = $_POST['oldPassword'];
-        //$newPassword = $_POST['newPassword'];
-        $conPassword = $_POST['conPassword'];
+// Сохранить данные по пользователю
+if (isset($_POST['saveProfilSettings'])) {
+    $user_id = $_SESSION['user_edit_obj_id'];
+    $first_name = $_POST['first_name'];
+    $last_name = $_POST['last_name'];
+    $email = $_POST['email'];
+    $login_instagram = $_POST['login_instagram'];
+    $phone = $auth->phoneReplace($_POST['phone']);
+    $user_role_id = $_POST['user_role'];
 
-        if (strlen($first_name) == 0 && strlen($last_name) == 0) {
-            //$_SESSION['errors'][] = 'Имя и фамилия должны быть заполнены';
-        }
-        if (strlen($email) < 2 && $auth->emailValid($email)) {
-            $_SESSION['errors'][] = 'Адрес электронной почты должен быть корректен';
-        }
+    //$oldPassword = $_POST['oldPassword'];
+    //$newPassword = $_POST['newPassword'];
+    $conPassword = $_POST['conPassword'];
+
+    if (strlen($first_name) == 0 && strlen($last_name) == 0) {
+        //$_SESSION['errors'][] = 'Имя и фамилия должны быть заполнены';
+    }
+    if (strlen($email) < 2 && $auth->emailValid($email)) {
+        $_SESSION['errors'][] = 'Адрес электронной почты должен быть корректен';
+    }
 //        if (strlen($phone) == 0) {
 //            $_SESSION['errors'][] = 'Номер телефона должен быть заполнен';
 //        }
 
-        $ret = false;
-        if (count($_SESSION['errors']) == 0) {
+    $ret = false;
+    if (count($_SESSION['errors']) == 0) {
 
-            if ($user_id > 0) {
+        if ($user_id > 0) {
 
 //                $select_query_email = "SELECT * FROM `zay_users` u WHERE u.`email`='?' and id<>'?'";
 //                $users_email = $user->getSelectArray($select_query_email, array($email, $user_id));
 //
 //                $select_query_phone = "SELECT * FROM `zay_users` u WHERE u.`phone`='?' and id<>'?'";
 //                $users_phone = $user->getSelectArray($select_query_phone, array($phone, $user_id));
-                //if (count($users_email) == 1) {
-                $updateUserInfo = "UPDATE `zay_users` SET `email`='?',`phone`='?',`first_name`='?',"
-                        . "`last_name`='?', `login_instagram`='?' WHERE id='?' ";
-                $ret = $user->query($updateUserInfo, array($email, $phone, $first_name, $last_name, $login_instagram, $user_id));
-                //} else {
-                //$ret = false;
-                //    $_SESSION['errors'][] = 'Ошибка! В системе будет 2 клиента с таким email адресом или номером телефона!';
-                //}
-            } else {
-                if (strlen($conPassword) > 2) {
-                    // Не зарегистрирован, регистрируем и вносим изменения
-                    if ($auth->register($email, $phone, $conPassword, $conPassword, 1, 1)) {
-                        if (isset($_SESSION['db_next_id'])) {
-                            $user_id = $_SESSION['db_next_id'];
-                            $updateUserInfo2 = "UPDATE `zay_users` SET `email`='?',`phone`='?',`first_name`='?',"
-                                    . "`last_name`='?', `login_instagram`='?' WHERE id='?' ";
-                            $ret = $user->query($updateUserInfo2, array($email, $phone, $first_name, $last_name, $login_instagram, $_SESSION['db_next_id']));
-                        }
-                    } else {
-                        $_SESSION['errors'][] = 'Ошибка регистрации!';
+            //if (count($users_email) == 1) {
+            $updateUserInfo = "UPDATE `zay_users` SET `email`='?',`phone`='?',`first_name`='?',"
+                    . "`last_name`='?', `login_instagram`='?' WHERE id='?' ";
+            $ret = $user->query($updateUserInfo, array($email, $phone, $first_name, $last_name, $login_instagram, $user_id));
+            //} else {
+            //$ret = false;
+            //    $_SESSION['errors'][] = 'Ошибка! В системе будет 2 клиента с таким email адресом или номером телефона!';
+            //}
+        } else {
+            if (strlen($conPassword) > 2) {
+                // Не зарегистрирован, регистрируем и вносим изменения
+                if ($auth->register($email, $phone, $conPassword, $conPassword, 1, 1)) {
+                    if (isset($_SESSION['db_next_id'])) {
+                        $user_id = $_SESSION['db_next_id'];
+                        $updateUserInfo2 = "UPDATE `zay_users` SET `email`='?',`phone`='?',`first_name`='?',"
+                                . "`last_name`='?', `login_instagram`='?' WHERE id='?' ";
+                        $ret = $user->query($updateUserInfo2, array($email, $phone, $first_name, $last_name, $login_instagram, $_SESSION['db_next_id']));
                     }
                 } else {
-                    $_SESSION['errors'][] = 'Не задан пароль!';
+                    $_SESSION['errors'][] = 'Ошибка регистрации!';
                 }
+            } else {
+                $_SESSION['errors'][] = 'Не задан пароль!';
             }
+        }
 
-            $user->edit_user_role($user_id, $user_role_id);
+        $user->edit_user_role($user_id, $user_role_id);
 
-            if (!$ret) {
-                $_SESSION['errors'][] = 'Обновление не выполнено';
-            }
+        if (!$ret) {
+            $_SESSION['errors'][] = 'Обновление не выполнено';
+        }
 
-            if (strlen($conPassword) > 3) {
-                $auth->updateUserPassword($conPassword, $user_id);
-            }
+        if (strlen($conPassword) > 3) {
+            $auth->updateUserPassword($conPassword, $user_id);
+        }
 //        if (strlen($oldPassword) > 0) {
 //            $obj = $user->getSelectArray("SELECT * FROM `zay_users` WHERE id='?' ", array($id))[0];
 //
@@ -119,108 +119,108 @@ if ($user->isEditor()) {
 //            }
 //        }
 
-            if (count($_SESSION['errors']) == 0) {
-                $result = array('success' => 1, 'success_text' => 'Данные обновлены');
-            }
+        if (count($_SESSION['errors']) == 0) {
+            $result = array('success' => 1, 'success_text' => 'Данные обновлены');
         }
     }
+}
 
 // Удалить пользователя
-    if (isset($_POST['userDelete'])) {
-        $user_id = $_POST['system_user_id'];
+if (isset($_POST['userDelete'])) {
+    $user_id = $_POST['system_user_id'];
 
-        $querySelect = "SELECT u.*, r.* FROM `zay_users` u "
-                . "left join `zay_roles_users` ru on ru.user_id=u.id  "
-                . "left join `zay_roles` r on r.id=ru.role_id "
-                . "WHERE u.`id`='?'";
-        $u = $user->getSelectArray($querySelect, array($user_id))[0];
-        if ($u['role_privilege'] > $_SESSION['user']['info']['role_privilege']) {
-            $_SESSION['errors'][] = 'У вас нет прав на удаление этого пользователя';
+    $querySelect = "SELECT u.*, r.* FROM `zay_users` u "
+            . "left join `zay_roles_users` ru on ru.user_id=u.id  "
+            . "left join `zay_roles` r on r.id=ru.role_id "
+            . "WHERE u.`id`='?'";
+    $u = $user->getSelectArray($querySelect, array($user_id))[0];
+    if ($u['role_privilege'] > $_SESSION['user']['info']['role_privilege']) {
+        $_SESSION['errors'][] = 'У вас нет прав на удаление этого пользователя';
+    } else {
+        if ($user->deleteUsed($user_id)) {
+            $result = array('success' => 1, 'success_text' => 'Упешно удалено');
+            $_SESSION['message'][] = 'Упешно удалено';
         } else {
-            if ($user->deleteUsed($user_id)) {
-                $result = array('success' => 1, 'success_text' => 'Упешно удалено');
-                $_SESSION['message'][] = 'Упешно удалено';
-            } else {
-                $_SESSION['errors'][] = 'Ошибка при удалении';
-            }
+            $_SESSION['errors'][] = 'Ошибка при удалении';
         }
     }
+}
 
-    if (isset($_POST['sortable'])) {
-        $json_data = json_decode($_POST['json_data']);
-        for ($i = 0; $i < count($json_data->data); $i++) {
-            //echo "P: {$json_data->data[$i]->content_id} \n";
-            $p->contentSorted($json_data->data[$i]->content_id, $json_data->data[$i]->sort);
-        }
+if (isset($_POST['sortable'])) {
+    $json_data = json_decode($_POST['json_data']);
+    for ($i = 0; $i < count($json_data->data); $i++) {
+        //echo "P: {$json_data->data[$i]->content_id} \n";
+        $p->contentSorted($json_data->data[$i]->content_id, $json_data->data[$i]->sort);
     }
+}
 
-    // Получение всех доступных ролей
-    if (isset($_POST['get_roles_array'])) {
-        $data = $user->get_roles_array();
-        $result = array('success' => 1, 'success_text' => '', 'data' => $data);
+// Получение всех доступных ролей
+if (isset($_POST['get_roles_array'])) {
+    $data = $user->get_roles_array();
+    $result = array('success' => 1, 'success_text' => '', 'data' => $data);
+}
+
+if (isset($_GET['add_new_role'])) {
+    if ($user->add_new_role()) {
+        $result = array('success' => 1, 'success_text' => 'Упешно добавлено');
+    } else {
+        $result = array('success' => 0, 'success_text' => 'Ошибка!');
     }
+}
 
-    if (isset($_GET['add_new_role'])) {
-        if ($user->add_new_role()) {
-            $result = array('success' => 1, 'success_text' => 'Упешно добавлено');
+// Генерация ссылки для быстрой авторизации
+if (isset($_POST['creat_fast_auth_link'])) {
+    if (isset($_POST['email'])) {
+        $code = $auth->set_code_integration($_POST['email']);
+        $result = array(
+            'success' => 1,
+            'success_text' => 'Успешно создана ссылка',
+            'data' => array('code' => "{$_SERVER['REQUEST_SCHEME']}://{$_SERVER['HTTP_HOST']}/auth/?code_integration=" . $code)
+        );
+    } else {
+        $result = array('success' => 0, 'success_text' => 'Не передан email адрес');
+    }
+}
+
+
+// отправить email сообщение 
+if (isset($_POST['userSendActivateEmail'])) {
+    if ($_POST['userSendActivateEmail'] > 0) {
+        $user_id = $_POST['userSendActivateEmail'];
+        $obj = $user->getSelectArray("SELECT * FROM `zay_users` where id='?'", array($user_id))[0];
+
+        //$pass_hash = $obj['u_pass'];//$auth->passHash($obj['u_pass']);
+
+        $activate_code = $auth->passHash(PRIVATE_CODE . $obj['email'] . time());
+        $activate_codeBase64 = base64_encode($activate_code);
+
+
+        $query = "UPDATE `zay_users` SET `active_code`='?' "
+                . "WHERE `id`='?' ";
+        $user->query($query, array($activate_code, $user_id));
+        if ($auth->sendActivateEmail($obj['email'], $activate_codeBase64, 'mail_activate_account')) {
+            $result = array('success' => 1, 'success_text' => 'Сообщение отправлено');
         } else {
             $result = array('success' => 0, 'success_text' => 'Ошибка!');
         }
+        return $result;
     }
-
-    // Генерация ссылки для быстрой авторизации
-    if (isset($_POST['creat_fast_auth_link'])) {
-        if (isset($_POST['email'])) {
-            $code = $auth->set_code_integration($_POST['email']);
-            $result = array(
-                'success' => 1,
-                'success_text' => 'Успешно создана ссылка',
-                'data' => array('code' => "{$_SERVER['REQUEST_SCHEME']}://{$_SERVER['HTTP_HOST']}/auth/?code_integration=" . $code)
-            );
-        } else {
-            $result = array('success' => 0, 'success_text' => 'Не передан email адрес');
-        }
+}
+/*
+ * Авторизация под другим пользователем
+ */
+if (isset($_POST['userAuth']) && $_POST['userAuth'] > 0) {
+    $_SESSION['user']['other'] = 1;
+    $user_id = $_POST['userAuth'];
+    $data = $user->getUserInfo($user_id);
+    $_SESSION['user']['other_info'] = $_SESSION['user']['info'];
+    $_SESSION['user']['info'] = $data;
+    if (strlen($_SESSION['user']['info']['avatar']) == 0) {
+        // Аватар по умолчанию
+        $_SESSION['user']['info']['avatar'] = '/assets/img/user/user.jpg';
     }
-
-
-    // отправить email сообщение 
-    if (isset($_POST['userSendActivateEmail'])) {
-        if ($_POST['userSendActivateEmail'] > 0) {
-            $user_id = $_POST['userSendActivateEmail'];
-            $obj = $user->getSelectArray("SELECT * FROM `zay_users` where id='?'", array($user_id))[0];
-
-            //$pass_hash = $obj['u_pass'];//$auth->passHash($obj['u_pass']);
-
-            $activate_code = $auth->passHash(PRIVATE_CODE . $obj['email'] . time());
-            $activate_codeBase64 = base64_encode($activate_code);
-
-
-            $query = "UPDATE `zay_users` SET `active_code`='?' "
-                    . "WHERE `id`='?' ";
-            $user->query($query, array($activate_code, $user_id));
-            if ($auth->sendActivateEmail($obj['email'], $activate_codeBase64, 'mail_activate_account')) {
-                $result = array('success' => 1, 'success_text' => 'Сообщение отправлено');
-            } else {
-                $result = array('success' => 0, 'success_text' => 'Ошибка!');
-            }
-            return $result;
-        }
-    }
-    /*
-     * Авторизация под другим пользователем
-     */
-    if (isset($_POST['userAuth']) && $_POST['userAuth'] > 0) {
-        $_SESSION['user']['other'] = 1;
-        $user_id = $_POST['userAuth'];
-        $data = $user->getUserInfo($user_id);
-        $_SESSION['user']['other_info'] = $_SESSION['user']['info'];
-        $_SESSION['user']['info'] = $data;
-        if (strlen($_SESSION['user']['info']['avatar']) == 0) {
-            // Аватар по умолчанию
-            $_SESSION['user']['info']['avatar'] = '/assets/img/user/user.jpg';
-        }
-        $user->isAccess(0);
-        $url = $_SESSION['location_href'];
+    $user->isAccess(0);
+    $url = $_SESSION['location_href'];
 //        if ($user->isAdmin() || $user->isEditor()) {
 //            $url = '/admin/';
 //            $r = 1;
@@ -232,8 +232,7 @@ if ($user->isEditor()) {
 //        if ($r == 0) {
 //            $url = '/';
 //        }
-        //echo 'url: ' . $url;
-        $result = array('success' => 1, 'success_text' => 'Успешно авторизирован', 'action' => $url, 'action_time' => '0');
-    }
+    //echo 'url: ' . $url;
+    $result = array('success' => 1, 'success_text' => 'Успешно авторизирован', 'action' => $url, 'action_time' => '0');
 }
 
