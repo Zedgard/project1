6e600479497d0f9044787ad2c7b3fd618430d263
Site <koman1706@gmail.com>
2021-12-19 18:57:21 +0300

    Refactoring extension webhook 3

--- extension/webhook/index.php
+++ extension/webhook/index.php
@@ -16,14 +16,16 @@ if(!empty($_POST) && isset($_POST['payment']))
 	include_once $_SERVER['DOCUMENT_ROOT'] . '/extension/products/inc.php';
 	include_once $_SERVER['DOCUMENT_ROOT'] . '/extension/auth/inc.php';
 	include_once $_SERVER['DOCUMENT_ROOT'] . '/extension/users/inc.php';
+	include_once $_SERVER['DOCUMENT_ROOT'] . '/extension/webhook/inc.php';
+	include_once $_SERVER['DOCUMENT_ROOT'] . '/extension/pay/inc.php';
 	include_once $_SERVER['DOCUMENT_ROOT'] . '/class/sqlLight.php';
-	$p_payment = json_decode($_POST['payment']);
+	// $p_pay = json_decode($_POST['payment']);
 	$p_products = new \project\products();
 	$p_user = new \project\user();
 	$auth = new \project\auth();
 	$sqlLight = new \project\sqlLight();
 	$webhook = new \project\webhook();
-	$sqlLight = new \project\sqlLight();
+	$p_pay = new \project\pay();
 	$product = null;//приобретаемый товар
 	$user = null;//пользователь
 	$pay_date = date("Y-m-d H:i:s");//текущая дата
@@ -35,16 +37,23 @@ if(!empty($_POST) && isset($_POST['payment']))
 	$pay_status = "succeeded";//статус платежа
 	$pay_descr = null;//описание платежа
 	$confirm_url = $_SERVER['HTTP_REFERER'];//адрес, откуда пришли данные
-	if(isset($p_payment['products']))//проверяем, передано ли название товара
+	if(!empty($_POST['payment']['products'])) //проверяем, передано ли название товара
 	{
-		foreach ($p_payment['products'] as $prod_name)//для каждого названия
+		if(is_array($_POST['payment']['products']))
 		{
-			$pay_descr .= $prod_name;//записываем название в таблицу
+			foreach ($_POST['payment']['products'] as $prod) //для каждого названия
+			{
+				$pay_descr .= $prod["name"]; //записываем название в таблицу
+			}
+		}
+		else
+		{
+			$pay_descr = $_POST['payment']['products'];
 		}
 	}
-	if(isset($p_payment['amount']))
+	if(!empty($_POST['payment']["amount"]))
 	{
-		$pay_sum = $p_payment['amount'];
+		$pay_sum = $_POST['payment']["amount"];
 	}
 	if(isset($_POST['paymentsystem']))
 	{
@@ -88,13 +97,15 @@ if(!empty($_POST) && isset($_POST['payment']))
 		$user = $p_user->user_info_by_phone_or_email($_POST['phone'],$_POST['email']);
 		// file_put_contents($_SERVER['DOCUMENT_ROOT']."/webhook.txt", json_encode($user));
 	}
-	// if(!empty($product) && !empty($user) && !empty($pay_type) && !empty($pay_sum) && !empty($pay_descr))
-	// {
-		// $result = $webhook->create_payment($max_id, $pay_type, $user['id'], $pay_sum, $pay_date, $pay_key, $pay_status, $pay_descr ,$confirmUrl);
-		$data = ['id' => $max_id, 'pay_type' => $pay_type, 'user_id' => $user['id'], 'pay_sum' => $pay_sum, 'pay_date' => $pay_date, 'pay_key' => $pay_key, 'pay_status' => $pay_status, 'pay_descr' => $pay_descr,'confirm_url' => $confirm_url];
-		file_put_contents($_SERVER['DOCUMENT_ROOT']."/webhook.txt", json_encode($data));
+	if(!empty($product) && !empty($user) && !empty($pay_type) && !empty($pay_sum) && !empty($pay_descr))
+	{
+		$p_payment = $webhook->create_payment($max_id, $pay_type, $user['id'], $pay_sum, $pay_date, $pay_key, $pay_status, $pay_descr ,$confirm_url);//добавляем оплату в базу
+		$res = $p_pay->insert_pay_products($p_payment['id'], $product['id'], $pay_sum);
+		// $data = array('id' => $max_id, 'pay_type' => $pay_type, 'user_id' => $user['id'], 'pay_sum' => $pay_sum, 'pay_date' => $pay_date, 'pay_key' => $pay_key, 'pay_status' => $pay_status, 'pay_descr' => $pay_descr,'confirm_url' => $confirm_url);
+		$result = json_encode($res);
+		file_put_contents($_SERVER['DOCUMENT_ROOT']."/webhook.txt", $result);
 		// file_put_contents($_SERVER['DOCUMENT_ROOT']."/webhook.txt", json_encode($p_payment));
-	// }
+	}
 
 
 }
